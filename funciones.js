e87aedc9d1a605a448562379988b777fd8a59b50
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-17 16:11:04 -0500

    feat: agregar función calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,9 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos){
+   let valorDisponible = 0;
+   valorDisponible = ingresos - egresos;
+    if(valorDisponible < 0){
+        return valorDisponible;
+   }
+    return valorDisponible;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,14 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+
+    let cmpIngresoFloat= document.getElementById("txtIngresos");
+    let ingresos = parseFloat(cmpIngresoFloat.value);
+
+    let cmpEngresosFloat = document.getElementById("txtEgresos");
+    let egresos =parseFloat(cmpEngresosFloat.value);
+    
+    let valorDisponible=calcularDisponible(ingresos,egresos);
+    let disponible=document.getElementById("spnDisponible");
+    disponible.innerText=valorDisponible;
+
+}
